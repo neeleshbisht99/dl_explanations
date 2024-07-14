fdeb903dfb2d0c2f6da39417bb30ee897924e136
Neelesh Bisht <neeleshbisht99@gmail.com>
2024-07-14 04:58:56 +0000

    moved model path to config

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ config = {
     'epochs': 100,
     'batch_size': 4,
     'img_size': 128,
-    'depth': 64
+    'depth': 64,
+    'model_path': "./models/archive/3d_cnn_scratch/3d_cnn_image_classification.pth"
 }
